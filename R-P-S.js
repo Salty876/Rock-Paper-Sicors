afb0cb4b4e976c2bbdf42560d1f4a35aceb44465
Salty876 <100157151+Salty876@users.noreply.github.com>
2022-04-20 19:46:48 -0600

    Added the player selection system
    The player makes a choice. They write down either, rock, paper or sissors. The string gets trimmed, lowerlettercased and returned to be used at a later moment.

--- R-P-S.js
+++ R-P-S.js
@@ -1,6 +1,6 @@
 let npcInput = Math.floor(Math.random()*3);
 
-let playerInput = prompt("Rock, Paper or Sizor?")
+
 function computerInput (){
     if (npcInput == 0) {
       
@@ -17,11 +17,20 @@ function computerInput (){
          return "Sizor"
     }
 }
-
+computerInput()
 let npcChoice = computerInput()
 
+function playerChoice (){
+    guyChoose = prompt("What are you going to play?")
+    let miniscule = guyChoose.toLowerCase()
+    let finalizedChoice = miniscule.trim()
+    return finalizedChoice
+
+}
+
+let finalPlayerChoice = playerChoice()
+
+console.log(finalPlayerChoice)
 
 
-console.log(npcInput)
-console.log(npcChoice)
 
